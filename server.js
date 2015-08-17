ab7ae996054d81b9925cd55e901ceb0e75423bef
Vasiliy Mazhekin <mazhekin@gmail.com>
2015-08-17 14:14:17 +0200

    adding stylus

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 var express = require('express');
 var path = require('path');
 var stylus = require('stylus');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
 
+function compile(str, path) {
+    return stylus(str).set('filename ', path);
+}
+
 app.set('views', path.join(__dirname, 'server/views'));
 app.set('view engine', 'jade');
+app.use(morgan('dev')); // log every request to the console
+app.use(bodyParser.json())
+app.use(stylus.middleware({
+    src: __dirname + '/public',
+    compile: compile
+}));
+app.use(express.static(__dirname + '/public'));
 
 app.get('*', function(req, res) {
     res.render('index');
