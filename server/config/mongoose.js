b6ca1010d380f974f92b8b4cacba1314e3e84dd4
Vasiliy Mazhekin <mazhekin@gmail.com>
2015-08-19 19:39:00 +0200

    secured passwords added

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var crypto = require('crypto');
 
 module.exports = function (config) {
     mongoose.connect(config.db);
@@ -11,15 +12,36 @@ module.exports = function (config) {
     var userSchema = mongoose.Schema({
         firstName: String,
         lastName: String,
-        username: String
+        username: String,
+        salt: String,
+        hashed_pwd: String
     });
+    userSchema.methods = {
+        authenticate: function(passwordToMatch) {
+            return hashPwd(this.salt, passwordToMatch) === this.hashed_pwd;
+        }
+    };
 
     var User = mongoose.model('User', userSchema);
 
     User.find({}).exec(function(err, collection) {
         if (collection.length === 0) {
-            User.create({firstName: 'Vasiliy', lastName: 'Mazhekin', username: 'mazhekin'});
-            User.create({firstName: 'Ivan', lastName: 'Ivanov', username: 'ivanov'});
+            var salt, hash;
+            salt = createSalt();
+            hash = hashPwd(salt, 'mazhekin');
+            User.create({firstName: 'Vasiliy', lastName: 'Mazhekin', username: 'mazhekin', salt: salt, hashed_pwd: hash});
+            salt = createSalt();
+            hash = hashPwd(salt, 'ivanov');
+            User.create({firstName: 'Ivan', lastName: 'Ivanov', username: 'ivanov', salt: salt, hashed_pwd: hash});
         }
     });
 };
+
+function createSalt() {
+    return crypto.randomBytes(128).toString('base64');
+}
+
+function hashPwd(salt, pwd) {
+    var hmac = crypto.createHmac('sha1', salt);
+    return hmac.update(pwd).digest('hex');
+}
