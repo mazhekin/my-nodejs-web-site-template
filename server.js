1818c9aecb361da4f17f75609815aafeb047bb52
Vasiliy Mazhekin <mazhekin@gmail.com>
2015-08-19 11:51:19 +0200

    server login code

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 var express = require('express');
+var mongoose = require('mongoose');
+
+var passport = require('passport');
+LocalStrategy = require('passport-local').Strategy;
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
@@ -9,6 +13,35 @@ var config = require('./server/config/config')['production'];
 require('./server/config/express')(app, config);
 
 require('./server/config/mongoose')(config);
+
+var User = mongoose.model('User');
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        User.findOne({username: username}).exec(function(err, user) {
+            if (user) {
+                return done(null, user);
+            } else {
+                return done(null, false);
+            }
+        });
+    }
+));
+
+passport.serializeUser(function(user, done) {
+    if (user) {
+        done(null, user._id);
+    }
+});
+
+passport.deserializeUser(function() {
+    User.findOne({_id:id}).exec(function(err, user){
+        if (user) {
+            return done(null, user);
+        } else {
+            return done(null, false);
+        }
+    });
+});
 
 require('./server/config/routes')(app);
 
--- server/config/express.js
+++ server/config/express.js
@@ -3,7 +3,9 @@ var stylus = require('stylus');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var path = require('path');
-
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var passport = require('passport');
 
 module.exports = function(app, config) {
     function compile(str, path) {
@@ -13,7 +15,11 @@ module.exports = function(app, config) {
     app.set('views', path.join(config.rootPath, 'server/views'));
     app.set('view engine', 'jade');
     app.use(morgan('dev')); // log every request to the console
+    app.use(cookieParser());
     app.use(bodyParser.json());
+    app.use(session({secret: 'my node js template secret'}));
+    app.use(passport.initialize());
+    app.use(passport.session());
     app.use(stylus.middleware({
         src: config.rootPath + '/public',
         compile: compile
