480239ba30464ff271397753964e6b394840e5f9
Vasiliy Mazhekin <mazhekin@gmail.com>
2015-08-19 11:59:58 +0200

    client simple login code

--- public/app/account/mvNavBarLoginCtrl.js
+++ public/app/account/mvNavBarLoginCtrl.js
@@ -1,5 +1,12 @@
-angular.module('app').controller('mvNavBarLoginCtrl', function($scope) {
+angular.module('app').controller('mvNavBarLoginCtrl', function($scope, $http) {
     $scope.signin = function(username, password) {
-        window.alert('not done');
+        $http.post('/login', {username: username, password: password})
+            .then(function(response){
+                if (response.data.success) {
+                    window.alert('logged in!');
+                } else {
+                    window.alert('logged in failed');
+                }
+            });
     };
 });
