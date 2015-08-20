51eceba52166b0a7e086bee5e40ff41a5bccad95
Vasiliy Mazhekin <mazhekin@gmail.com>
2015-08-20 13:39:37 +0200

    passport strategy refactoring

--- server.js
+++ server.js
@@ -1,8 +1,4 @@
 var express = require('express');
-var mongoose = require('mongoose');
-
-var passport = require('passport');
-LocalStrategy = require('passport-local').Strategy;
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
@@ -14,39 +10,7 @@ require('./server/config/express')(app, config);
 
 require('./server/config/mongoose')(config);
 
-var User = mongoose.model('User');
-passport.use(new LocalStrategy(
-    function(username, password, done) {
-        User.findOne({username: username}).exec(function(err, user) {
-            if (user && user.authenticate(password)) {
-                return done(null, user);
-            } else {
-                return done(null, false);
-            }
-        });
-    }
-));
-/*
-app.use(function(req, res, next) {
-    console.log(req.user);
-    next();
-});
-*/
-passport.serializeUser(function(user, done) {
-    if (user) {
-        done(null, user._id);
-    }
-});
-
-passport.deserializeUser(function(id, done) {
-    User.findOne({_id:id}).exec(function(err, user){
-        if (user) {
-            return done(null, user);
-        } else {
-            return done(null, false);
-        }
-    });
-});
+require('./server/config/passport')();
 
 require('./server/config/routes')(app);
 
---
+++ server/config/passport.js
@@ -0,0 +1,40 @@
+var mongoose = require('mongoose');
+var passport = require('passport');
+LocalStrategy = require('passport-local').Strategy;
+var User = mongoose.model('User');
+
+module.exports = function() {
+
+    passport.use(new LocalStrategy(
+        function(username, password, done) {
+            User.findOne({username: username}).exec(function(err, user) {
+                if (user && user.authenticate(password)) {
+                    return done(null, user);
+                } else {
+                    return done(null, false);
+                }
+            });
+        }
+    ));
+    /*
+     app.use(function(req, res, next) {
+     console.log(req.user);
+     next();
+     });
+     */
+    passport.serializeUser(function(user, done) {
+        if (user) {
+            done(null, user._id);
+        }
+    });
+
+    passport.deserializeUser(function(id, done) {
+        User.findOne({_id:id}).exec(function(err, user){
+            if (user) {
+                return done(null, user);
+            } else {
+                return done(null, false);
+            }
+        });
+    });
+};
