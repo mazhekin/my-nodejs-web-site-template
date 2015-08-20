bb8927914b2d395bfbcd0291f883fe7cb1fcb6fe
Vasiliy Mazhekin <mazhekin@gmail.com>
2015-08-20 17:35:45 +0200

    protecting admin page

--- public/app/app.js
+++ public/app/app.js
@@ -3,7 +3,26 @@ angular.module('app', ['ngResource', 'ngRoute']);
 angular.module('app').config(function($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
     $routeProvider
-        .when('/', { templateUrl: '/partials/main/main', controller: 'mvMainCtrl'});
+        .when('/', { templateUrl: '/partials/main/main', controller: 'mvMainCtrl'})
+        .when('/admin/users', { templateUrl: '/partials/admin/user-list',
+            controller: 'mvUserListCtrl',
+            resolve: {
+                auth: function(mvIdentity, $q) {
+                    if (mvIdentity.currentUser && mvIdentity.currentUser.roles.indexOf('admin') > -1) {
+                        return true;
+                    } else {
+                        return $q.reject('not authorized');
+                    }
+                }
+            }
+        });
 
 });
 
+angular.module('app').run(function($rootScope, $location) {
+    $rootScope.$on('$routeChangeError', function(event, current, previous, rejection) {
+        if (rejection === 'not authorized') {
+            $location.path('/');
+        }
+    });
+});
